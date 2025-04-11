01b7f3e43216888039ef3b992bc27d27155656c1
DokiDokiPB <20497934+a1091150@users.noreply.github.com>
2025-04-11 13:33:38 +0800

    Add metal-cpp into pybind

--- addExample/addArray.cpp
+++ addExample/addArray.cpp
@@ -1,9 +1,16 @@
 
 #include "addArray.h"
 #include <torch/extension.h>
-// #include <Foundation/Foundation.hpp>
-// #include <Metal/Metal.hpp>
-// #include <QuartzCore/QuartzCore.hpp>
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+
+void testMetalCpp() {
+    const void* strings[] = {CFSTR("Hello"), CFSTR("Hello2"), CFSTR("Hello3")};
+    CFArrayRef array = CFArrayCreate(kCFAllocatorDefault, strings, 3, &kCFTypeArrayCallBacks);
+    CFShow(array);
+    CFRelease(array);
+}
 
 void addArray(torch::Tensor& input) {
     input.add_(1);
@@ -11,4 +18,5 @@ void addArray(torch::Tensor& input) {
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("add", &addArray);
+    m.def("testMetalCpp", &testMetalCpp);
 }
--- addExample/main.py
+++ addExample/main.py
@@ -5,8 +5,13 @@ import torch.utils.cpp_extension
 compiled_lib = torch.utils.cpp_extension.load(
     name="AddArray",
     sources=["addArray.cpp"],
-    extra_cflags=['-std=c++17']
+    extra_include_paths=['../metal-cpp'],
+    extra_cflags=['-std=c++17', '-framework Foundation', '-framework Metal', '-framework QuartzCore']
 )
+
+def testMetalCpp():
+    compiled_lib.testMetalCpp()
+    pass
 
 def testAdd(foo: torch.Tensor) -> torch.Tensor:
     compiled_lib.add(foo)
@@ -16,4 +21,5 @@ if __name__ == "__main__":
     foo = torch.ones([1, 2])
     testAdd(foo=foo)
     print(foo)
+    testMetalCpp()
     pass
